fbc1c419d3d011fed981d3c4e0684d79ead68118
Jason Zhang <jasonzhang@welove-inc.com>
2020-04-23 22:29:37 +0800

    feat: 暴力超时，使用动态规划解决。

--- src/com/bitxiaoxia/leetcode/daily20200423/SolutionCoinIccl.java
+++ src/com/bitxiaoxia/leetcode/daily20200423/SolutionCoinIccl.java
@@ -5,35 +5,48 @@ package com.bitxiaoxia.leetcode.daily20200423;
  * @date 2020/4/23 17:53
  */
 public class SolutionCoinIccl {
-	public static int waysToChange(int n) {
-		n = n - n % 5;
-		int[] coins = {25, 10, 5, 1};
-		long result = 0;
+	private static final int mod = 1000000007;
+	private static final int[] coins = {25,10,5,1};
 
-		int num25 = n / 25;
-		for (int j = num25; j >= 0; j--) {
-			int remain25 = n - j * 25;
-			int num10 = remain25 / 10;
-			for (int k = num10; k >= 0; k--) {
-				int remain10 = remain25 - k * 10;
-				int num5 = remain10 / 5;
-				result += (num5 + 1);
+    public static int waysToChange(int n) {
+		int[] res = new int[n + 1];
+		res[0] = 1;
+		for(int coin : coins){
+			for(int i = coin;i <= n;i++){
+				res[i] = (res[i] + res[i - coin]) % mod;
 			}
 		}
-		result = result % 1000000007;
-		return (int) result;
-	}
+		return res[n];
+    }
 
-	public static void main(String[] args) {
-		int n = 0;
-		for (int i = 1; i < 200; i += 5) {
-			int m = waysToChange(i);
-			System.out.println(i + " " + m + " " + (m - n));
-			n = m;
-			if (i == 1) {
-				i--;
-			}
-		}
+    private static int wayToChangeForceCal(int n) {
+        n = n - n % 5;
+        int[] coins = {25, 10, 5, 1};
+        long result = 0;
 
-	}
+        int num25 = n / 25;
+        for (int j = num25; j >= 0; j--) {
+            int remain25 = n - j * 25;
+            int num10 = remain25 / 10;
+            for (int k = num10; k >= 0; k--) {
+                int remain10 = remain25 - k * 10;
+                int num5 = remain10 / 5;
+                result += (num5 + 1);
+            }
+        }
+        result = result % 1000000007;
+        return (int) result;
+    }
+
+    public static void main(String[] args) {
+        int n = 0;
+        for (int i = 1; i < 200; i += 5) {
+            int m = waysToChange(i);
+            System.out.println(i + " " + m + " " + (m - n));
+            n = m;
+            if (i == 1) {
+                i--;
+            }
+        }
+    }
 }
